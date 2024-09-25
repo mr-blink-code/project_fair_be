4684287d9b28c9e47c51017955b54a2c78bc0cd7
mr-blink-code <tmmahesh007@gmail.com>
2024-09-25 13:39:27 +0530

    third commit

--- Controllers/projectController.js
+++ Controllers/projectController.js
@@ -1,29 +1,30 @@
-const projects = require("../Models/projectCollection")
+const projects = require("../Models/projectCollection");
 
-exports.addproject= async(req,res)=>{
-    console.log("inside add project")
-    const userId = req.payload;
-    console.log("userId:-" ,userId)
-    const projectImage=req.file.filename
-    console.log("inage file name",projectImage)
-    const {title,language,github,website,overview}=rq.body;
-    try{
-        const exixtingProjects = await projects.findOne({github:github});
-        if(exixtingProjects){
-            res.statud(409).json("Project already exists")
-        }
-        else{
-            const newProject = new projects({
-                title,
-                language,
-                github,
-                website,
-                overview,
-                projectImage
-            })
-        }
+exports.addproject = async (req, res) => {
+  console.log("inside add project");
+  const userId = req.payload;
+  console.log("userId:-", userId);
+  const projectImage = req.file.filename;
+  console.log("inage file name", projectImage);
+  const { title, language, github, website, overview } = req.body;
+  try {
+    const exixtingProjects = await projects.findOne({ github: github });
+    if (exixtingProjects) {
+      res.status(409).json("Project already exists");
+    } else {
+      const newProject = new projects({
+        title,
+        language,
+        github,
+        website,
+        overview,
+        projectImage,
+        userId,
+      });
+      await newProject.save();
+      res.status(200).json("project uploded sucessfully");
     }
-    catch(err){
-        res.status(401).json("project upload failed")
-    }
-}
+  } catch (err) {
+    res.status(401).json("project upload failed");
+  }
+};
